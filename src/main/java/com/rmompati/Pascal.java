8388b6b100e9b4dbaf26c9319ff3ef74155a13bb
RMompati <mmpterold@gmail.com>
2023-05-25 15:28:10 +0200

    Initial commit.

---
+++ src/main/java/com/rmompati/Pascal.java
@@ -0,0 +1,12 @@
+package com.rmompati;
+
+/**
+ * Entry point of the interpreter.
+ *
+ */
+public class Pascal {
+  public static void main( String[] args )
+  {
+    System.out.println( "Welcome!" );
+  }
+}
