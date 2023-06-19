7c72961a9b02ea64f17dc1747e302b665ac4f70f
RMompati <mmpterold@gmail.com>
2023-06-19 20:18:45 +0200

    Implement the parse method the repeat statement parser.

--- src/main/java/com/rmompati/lang/pascal/frontend/parsers/RepeatStatementParser.java
+++ src/main/java/com/rmompati/lang/pascal/frontend/parsers/RepeatStatementParser.java
@@ -1,6 +1,15 @@
 package com.rmompati.lang.pascal.frontend.parsers;
 
+import com.rmompati.lang.frontend.Token;
+import com.rmompati.lang.intermediate.ICodeFactory;
+import com.rmompati.lang.intermediate.ICodeNode;
 import com.rmompati.lang.pascal.frontend.PascalParserTD;
+import com.rmompati.lang.pascal.frontend.PascalTokenType;
+
+import static com.rmompati.lang.intermediate.icodeimpl.ICodeNodeTypeImpl.LOOP;
+import static com.rmompati.lang.intermediate.icodeimpl.ICodeNodeTypeImpl.TEST;
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.UNTIL;
+import static com.rmompati.lang.pascal.frontend.error.PascalErrorCode.MISSING_UNTIL;
 
 /**
  * <h1>RepeatStatementParser</h1>
@@ -15,5 +24,33 @@ public class RepeatStatementParser extends StatementParser {
    */
   public RepeatStatementParser(PascalParserTD parent) {
     super(parent);
+  }
+
+  /**
+   * Parses a REPEAT statement.
+   *
+   * @param token the initial token.
+   * @return the root node of the generated parse tree.
+   * @throws Exception if an error occurs.
+   */
+  @Override
+  public ICodeNode parse(Token token) throws Exception {
+    token = currentToken();
+
+    // Create the LOOP and TEST nodes.
+    ICodeNode loopNode = ICodeFactory.createICodeNode(LOOP);
+    ICodeNode testNode = ICodeFactory.createICodeNode(TEST);
+
+    // Parse the statement list terminated by the UNTIL token.
+    StatementParser statementParser = new RepeatStatementParser(this);
+    statementParser.parseList(token, loopNode, UNTIL, MISSING_UNTIL);
+    token = currentToken();
+
+    // Parse the expression. The TEST node adopts the expression subtree as its only child.
+    ExpressionParser expressionParser = new ExpressionParser(this);
+    testNode.addChild(expressionParser.parse(token));
+    loopNode.addChild(testNode);
+
+    return loopNode;
   }
 }
