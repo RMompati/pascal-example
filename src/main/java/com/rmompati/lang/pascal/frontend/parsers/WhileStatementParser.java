a1fe910e631f695a119c4dd69c8a9bfacb3863ce
RMompati <mmpterold@gmail.com>
2023-06-20 15:32:35 +0200

    Implement the parse method the while statement parser.

--- src/main/java/com/rmompati/lang/pascal/frontend/parsers/WhileStatementParser.java
+++ src/main/java/com/rmompati/lang/pascal/frontend/parsers/WhileStatementParser.java
@@ -1,6 +1,16 @@
 package com.rmompati.lang.pascal.frontend.parsers;
 
+import com.rmompati.lang.frontend.Token;
+import com.rmompati.lang.intermediate.ICodeFactory;
+import com.rmompati.lang.intermediate.ICodeNode;
 import com.rmompati.lang.pascal.frontend.PascalParserTD;
+import com.rmompati.lang.pascal.frontend.PascalTokenType;
+
+import java.util.EnumSet;
+
+import static com.rmompati.lang.intermediate.icodeimpl.ICodeNodeTypeImpl.*;
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.DO;
+import static com.rmompati.lang.pascal.frontend.error.PascalErrorCode.MISSING_DO;
 
 /**
  * <h1>WhileStatementParser</h1>
@@ -8,6 +18,15 @@ import com.rmompati.lang.pascal.frontend.PascalParserTD;
  * <p>A parser for the WHILE statement</p>
  */
 public class WhileStatementParser extends StatementParser {
+
+  // Synchronizing set for DO.
+  private static final EnumSet<PascalTokenType> DO_SET =
+      StatementParser.STMT_START_SET.clone();
+  static {
+    DO_SET.add(DO);
+    DO_SET.addAll(StatementParser.STMT_FOLLOW_SET);
+  }
+
   /**
    * Constructor.
    *
@@ -15,5 +34,46 @@ public class WhileStatementParser extends StatementParser {
    */
   public WhileStatementParser(PascalParserTD parent) {
     super(parent);
+  }
+
+  /**
+   * Parses a WHILE statement.
+   *
+   * @param token the initial token.
+   * @return the root node of the generated parse tree.
+   * @throws Exception if an error occurs.
+   */
+  @Override
+  public ICodeNode parse(Token token) throws Exception {
+    token = nextToken(); // Consume the "WHILE".
+
+    // Create "LOOP", "TEST", and "NOT" nodes.
+    ICodeNode loopNode = ICodeFactory.createICodeNode(LOOP);
+    ICodeNode breakNode = ICodeFactory.createICodeNode(TEST);
+    ICodeNode notNode = ICodeFactory.createICodeNode(NOT);
+
+    // The "LOOP" node adopts the "TEST" node as its first child.
+    // The "TEST" node adopts the "NOT" node as its only child.
+    loopNode.addChild(breakNode);
+    breakNode.addChild(notNode);
+
+    // Parse the expression.
+    // The "NOT" node adopts the expression subtree as its only child.
+    ExpressionParser expressionParser = new ExpressionParser(this);
+    notNode.addChild(expressionParser.parse(token));
+
+    // Synchronize at the "DO".
+    token = synchronize(DO_SET);
+    if (token.getType() == DO) {
+      token = nextToken(); // Consume the "DO".
+    } else {
+      errorHandler.flag(token, MISSING_DO, this);
+    }
+
+    // Parse the statement.
+    StatementParser statementParser = new StatementParser(this);
+    loopNode.addChild(statementParser.parse(token));
+
+    return loopNode;
   }
 }
