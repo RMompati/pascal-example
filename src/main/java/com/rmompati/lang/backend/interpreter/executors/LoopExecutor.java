253f68914eaec9e835e79da097e88b167130e16f
RMompati <mmpterold@gmail.com>
2023-06-27 12:21:41 +0200

    Implement the executor for loop statement

--- src/main/java/com/rmompati/lang/backend/interpreter/executors/LoopExecutor.java
+++ src/main/java/com/rmompati/lang/backend/interpreter/executors/LoopExecutor.java
@@ -1,6 +1,12 @@
 package com.rmompati.lang.backend.interpreter.executors;
 
 import com.rmompati.lang.backend.interpreter.Executor;
+import com.rmompati.lang.intermediate.ICodeNode;
+import com.rmompati.lang.intermediate.icodeimpl.ICodeNodeTypeImpl;
+
+import java.util.List;
+
+import static com.rmompati.lang.intermediate.icodeimpl.ICodeNodeTypeImpl.TEST;
 
 /**
  * <h1>LoopExecutor</h1>
@@ -16,5 +22,45 @@ public class LoopExecutor extends StatementExecutor {
    */
   public LoopExecutor(Executor parent) {
     super(parent);
+  }
+
+  /**
+   * Execute a loop statement.
+   *
+   * @param node the root node of the statement.
+   * @return null
+   */
+  @Override
+  public Object execute(ICodeNode node) {
+    boolean exitLoop = false;
+    ICodeNode exprNode = null;
+    List<ICodeNode> loopChildren = node.getChildren();
+
+    ExpressionExecutor expressionExecutor = new ExpressionExecutor(this);
+    StatementExecutor statementExecutor = new StatementExecutor(this);
+
+    // Loop until the "TEST" value is true.
+    while (!exitLoop) {
+      ++executionCount;
+
+      // Execute the children of the loop.
+      for (ICodeNode child : loopChildren) {
+        ICodeNodeTypeImpl childType = (ICodeNodeTypeImpl) child.getType();
+
+        // TEST node?
+        if (childType == TEST) {
+          if (exprNode == null) {
+            exprNode = child.getChildren().get(0);
+          }
+          exitLoop = (Boolean) expressionExecutor.execute(exprNode);
+        } else {
+          statementExecutor.execute(child);
+        }
+
+        // Exit if the TEST expression value is true.
+        if (exitLoop) break;
+      }
+    }
+    return null;
   }
 }
