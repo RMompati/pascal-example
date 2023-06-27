39479c69d908c02d138210460c9e332bc7b1e88f
RMompati <mmpterold@gmail.com>
2023-06-27 15:17:58 +0200

    Implement the executor for the if statement

--- src/main/java/com/rmompati/lang/backend/interpreter/executors/IfExecutor.java
+++ src/main/java/com/rmompati/lang/backend/interpreter/executors/IfExecutor.java
@@ -1,6 +1,9 @@
 package com.rmompati.lang.backend.interpreter.executors;
 
 import com.rmompati.lang.backend.interpreter.Executor;
+import com.rmompati.lang.intermediate.ICodeNode;
+
+import java.util.List;
 
 /**
  * <h1>IfExecutor</h1>
@@ -16,5 +19,34 @@ public class IfExecutor extends StatementExecutor {
    */
   public IfExecutor(Executor parent) {
     super(parent);
+  }
+
+  /**
+   * Executes an if statement.
+   *
+   * @param node the root node of the statement.
+   * @return null
+   */
+  @Override
+  public Object execute(ICodeNode node) {
+    // Get the "IF" node's children
+    List<ICodeNode> children = node.getChildren();
+    ICodeNode exprNode = children.get(0);
+    ICodeNode thenStmtNode = children.get(1);
+    ICodeNode elseStmtNode = children.size() > 2 ? children.get(2) : null;
+
+    ExpressionExecutor expressionExecutor = new ExpressionExecutor(this);
+    StatementExecutor statementExecutor = new StatementExecutor(this);
+
+    // Evaluate the expression to determine which statement to execute.
+    boolean b = (Boolean) expressionExecutor.execute(exprNode);
+    if (b) {
+      statementExecutor.execute(thenStmtNode);
+    } else if (elseStmtNode != null) {
+      statementExecutor.execute(elseStmtNode);
+    }
+
+    ++executionCount;
+    return null;
   }
 }
