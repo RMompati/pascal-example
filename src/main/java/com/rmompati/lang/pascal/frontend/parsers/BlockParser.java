0a5a13e240630abc9239be3c319541979f8eda11
RMompati <mmpterold@gmail.com>
2023-08-10 14:58:47 +0200

    Define the parse methods of the block parser and the declarations parser.

--- src/main/java/com/rmompati/lang/pascal/frontend/parsers/BlockParser.java
+++ src/main/java/com/rmompati/lang/pascal/frontend/parsers/BlockParser.java
@@ -1,6 +1,17 @@
 package com.rmompati.lang.pascal.frontend.parsers;
 
+import com.rmompati.lang.frontend.Token;
+import com.rmompati.lang.frontend.TokenType;
+import com.rmompati.lang.intermediate.ICodeFactory;
+import com.rmompati.lang.intermediate.ICodeNode;
+import com.rmompati.lang.intermediate.SymTableEntry;
 import com.rmompati.lang.pascal.frontend.PascalParserTD;
+
+import static com.rmompati.lang.intermediate.icodeimpl.ICodeNodeTypeImpl.COMPOUND;
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.BEGIN;
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.END;
+import static com.rmompati.lang.pascal.frontend.error.PascalErrorCode.MISSING_BEGIN;
+import static com.rmompati.lang.pascal.frontend.error.PascalErrorCode.MISSING_END;
 
 public class BlockParser extends PascalParserTD {
 
@@ -11,5 +22,38 @@ public class BlockParser extends PascalParserTD {
    */
   public BlockParser(PascalParserTD parent) {
     super(parent);
+  }
+
+  /**
+   * Parses a block.
+   * @param token the initial token.
+   * @param routineId the symbol table entry of the routine name.
+   * @return the root node of the parse tree.
+   * @throws Exception if an error occurs.
+   */
+  public ICodeNode parse(Token token, SymTableEntry routineId) throws Exception {
+    DeclarationsParser declarationsParser = new DeclarationsParser(this);
+    StatementParser statementParser = new StatementParser(this);
+
+    // Parse any declarations
+    declarationsParser.parse(token);
+
+    token = synchronize(StatementParser.STMT_START_SET);
+    TokenType tokenType = token.getType();
+    ICodeNode rootNode = null;
+
+    // Look for the "BEGIN" token to parse a compound statement.
+    if (token.getType() == BEGIN) {
+      rootNode = statementParser.parse(token);
+
+    } else { // Missing BEGIN: Attempt to parse anyway if possible.
+      errorHandler.flag(token, MISSING_BEGIN, this);
+
+      if (StatementParser.STMT_START_SET.contains(tokenType)) {
+        rootNode = ICodeFactory.createICodeNode(COMPOUND);
+        statementParser.parseList(token, rootNode, END, MISSING_END);
+      }
+    }
+    return rootNode;
   }
 }
--- src/main/java/com/rmompati/lang/pascal/frontend/parsers/DeclarationsParser.java
+++ src/main/java/com/rmompati/lang/pascal/frontend/parsers/DeclarationsParser.java
@@ -1,8 +1,30 @@
 package com.rmompati.lang.pascal.frontend.parsers;
 
+import com.rmompati.lang.frontend.Token;
 import com.rmompati.lang.pascal.frontend.PascalParserTD;
+import com.rmompati.lang.pascal.frontend.PascalTokenType;
+
+import java.util.EnumSet;
+
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.*;
 
 public class DeclarationsParser extends PascalParserTD {
+
+  static final EnumSet<PascalTokenType> DECLARATIONS_START_SET = EnumSet.of(CONST, TYPE, VAR, PROCEDURE);
+  static final EnumSet<PascalTokenType> TYPE_START_SET = EnumSet.of(CONST, TYPE, VAR, PROCEDURE);
+  static {
+    TYPE_START_SET.remove(CONST);
+  }
+
+  static final EnumSet<PascalTokenType> VAR_START_SET = EnumSet.of(CONST, TYPE, VAR, PROCEDURE);
+  static {
+    VAR_START_SET.remove(TYPE);
+  }
+
+  static final EnumSet<PascalTokenType> ROUTINE_START_SET = EnumSet.of(CONST, TYPE, VAR, PROCEDURE);
+  static {
+    ROUTINE_START_SET.remove(VAR);
+  }
 
   /**
    * Constructor.
@@ -11,5 +33,38 @@ public class DeclarationsParser extends PascalParserTD {
    */
   public DeclarationsParser(PascalParserTD parent) {
     super(parent);
+  }
+
+  /**
+   * Parses declarations.
+   * To be overridden by the specialized declarations parser subclasses.
+   * @param token the initial token.
+   * @throws Exception if an error occurs.
+   */
+  public void parse(Token token) throws Exception {
+    token = synchronize(DECLARATIONS_START_SET);
+
+    if (token.getType() == CONST) {
+      token = nextToken(); // Consume 'CONST'
+      ConstantsDefinitionParser constantsDefinitionParser = new ConstantsDefinitionParser(this);
+      constantsDefinitionParser.parse(token);
+    }
+
+    token = synchronize(TYPE_START_SET);
+    if (token.getType() == TYPE) {
+      token = nextToken();  // Consume 'TYPE'
+      TypeDefinitionParser typeDefinitionParser = new TypeDefinitionParser(this);
+      typeDefinitionParser.parse(token);
+    }
+
+    token = synchronize(VAR_START_SET);
+    if (token.getType() == VAR) {
+      token = nextToken();  // Consume 'VAR'
+      VariableDeclarationsParser variableDeclarationsParser = new VariableDeclarationsParser(this);
+//      variableDeclarationsParser.setDefinition(VARIABLE);
+      variableDeclarationsParser.parse(token);
+    }
+
+    token = synchronize(ROUTINE_START_SET);
   }
 }
