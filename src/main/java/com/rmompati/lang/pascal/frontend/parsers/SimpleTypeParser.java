93ddf32d2d7527047bad2250a605b3b290f98a46
RMompati <mmpterold@gmail.com>
2023-09-12 10:55:44 +0200

    Update Type Specification and Simple Type Parsers - implement theie parse methods.

--- src/main/java/com/rmompati/lang/pascal/frontend/parsers/SimpleTypeParser.java
+++ src/main/java/com/rmompati/lang/pascal/frontend/parsers/SimpleTypeParser.java
@@ -1,8 +1,27 @@
 package com.rmompati.lang.pascal.frontend.parsers;
 
+import com.rmompati.lang.frontend.Token;
+import com.rmompati.lang.intermediate.Definition;
+import com.rmompati.lang.intermediate.SymTableEntry;
+import com.rmompati.lang.intermediate.TypeSpec;
+import com.rmompati.lang.intermediate.symtableimpl.DefinitionImpl;
 import com.rmompati.lang.pascal.frontend.PascalParserTD;
+import com.rmompati.lang.pascal.frontend.PascalTokenType;
+
+import java.util.EnumSet;
+
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.*;
+import static com.rmompati.lang.pascal.frontend.error.PascalErrorCode.*;
 
 public class SimpleTypeParser extends TypeSpecificationParser {
+
+  // Synchronization set for starting a simple type specification
+  static final EnumSet<PascalTokenType> SIMPLE_START_SET = ConstantsDefinitionParser.CONSTANT_START_SET.clone();
+  static {
+    SIMPLE_START_SET.add(LEFT_PAREN);
+    SIMPLE_START_SET.add(COMMA);
+    SIMPLE_START_SET.add(SEMICOLON);
+  }
 
   /**
    * Constructor.
@@ -11,5 +30,59 @@ public class SimpleTypeParser extends TypeSpecificationParser {
    */
   public SimpleTypeParser(PascalParserTD parent) {
     super(parent);
+  }
+
+  /**
+   * Parses a Pascal type specification.
+   *
+   * @param token the current token.
+   * @return the type specification.
+   * @throws Exception if an error occurs.
+   */
+  @Override
+  public TypeSpec parse(Token token) throws Exception {
+    // Synchronize at the start of a simple type specification.
+    token = synchronize(SIMPLE_START_SET);
+
+    switch ((PascalTokenType) token.getType()) {
+      case IDENTIFIER: {
+        String name = token.getText().toLowerCase();
+        SymTableEntry id = symTabStack.lookup(name);
+
+        if (id != null) {
+          Definition definition = id.getDefinition();
+          if (definition == DefinitionImpl.TYPE) {
+            id.appendLineNumber(token.getLineNum());
+            token = nextToken(); // Consume the identifier.
+
+            return id.getTypeSpec();
+          } else if ((definition != DefinitionImpl.CONSTANT) && (definition != DefinitionImpl.ENUMERATION_CONSTANT)) {
+            errorHandler.flag(token, NOT_TYPE_IDENTIFIER, this);
+            token = nextToken(); // Consume the identifier.
+            return null;
+          } else {
+            SubrangeTypeParser subrangeTypeParser = new SubrangeTypeParser(this);
+            return subrangeTypeParser.parse(token);
+          }
+        } else {
+          errorHandler.flag(token, IDENTIFIER_UNDEFINED, this);
+          token = nextToken(); // Consume the identifier.
+          return null;
+        }
+      }
+      case LEFT_PAREN: {
+        EnumerationTypeParser enumerationTypeParser = new EnumerationTypeParser(this);
+        return enumerationTypeParser.parse(token);
+      }
+      case COMMA:
+      case SEMICOLON: {
+        errorHandler.flag(token, INVALID_TYPE, this);
+        return null;
+      }
+      default: {
+        SubrangeTypeParser subrangeTypeParser = new SubrangeTypeParser(this);
+        return subrangeTypeParser.parse(token);
+      }
+    }
   }
 }
--- src/main/java/com/rmompati/lang/pascal/frontend/parsers/TypeSpecificationParser.java
+++ src/main/java/com/rmompati/lang/pascal/frontend/parsers/TypeSpecificationParser.java
@@ -3,9 +3,19 @@ package com.rmompati.lang.pascal.frontend.parsers;
 import com.rmompati.lang.frontend.Token;
 import com.rmompati.lang.intermediate.TypeSpec;
 import com.rmompati.lang.pascal.frontend.PascalParserTD;
+import com.rmompati.lang.pascal.frontend.PascalTokenType;
+
+import java.util.EnumSet;
 
 public class TypeSpecificationParser extends PascalParserTD {
 
+  // Synchronization set for starting a type specification.
+  static final EnumSet<PascalTokenType> TYPE_START_SET = SimpleTypeParser.SIMPLE_START_SET.clone();
+  static {
+    TYPE_START_SET.add(PascalTokenType.ARRAY);
+    TYPE_START_SET.add(PascalTokenType.RECORD);
+    TYPE_START_SET.add(PascalTokenType.SEMICOLON);
+  }
   /**
    * Constructor.
    *
@@ -15,7 +25,28 @@ public class TypeSpecificationParser extends PascalParserTD {
     super(parent);
   }
 
-  public TypeSpec parse(Token token) {
-    return null;
+  /**
+   * Parses a Pascal type specification.
+   * @param token the current token.
+   * @return the type specification.
+   * @throws Exception if an error occurs.
+   */
+  public TypeSpec parse(Token token) throws Exception {
+    // Synchronize at the start of a type specification.
+    token = synchronize(TYPE_START_SET);
+    switch ((PascalTokenType) token.getType()) {
+      case ARRAY: {
+        ArrayTypeParser arrayTypeParser = new ArrayTypeParser(this);
+        return arrayTypeParser.parse(token);
+      }
+      case RECORD: {
+        RecordTypeParser recordTypeParser = new RecordTypeParser(this);
+        return recordTypeParser.parse(token);
+      }
+      default: {
+        SimpleTypeParser simpleTypeParser = new SimpleTypeParser(this);
+        return simpleTypeParser.parse(token);
+      }
+    }
   }
 }
