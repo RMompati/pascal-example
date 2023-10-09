7c5618a9e925fa0d91d51750f7b3dfb54010744a
RMompati <mmpterold@gmail.com>
2023-10-09 14:02:56 +0200

    Implement the parse methods for record type and variable declaration parsers.

--- src/main/java/com/rmompati/lang/pascal/frontend/parsers/RecordTypeParser.java
+++ src/main/java/com/rmompati/lang/pascal/frontend/parsers/RecordTypeParser.java
@@ -1,8 +1,29 @@
 package com.rmompati.lang.pascal.frontend.parsers;
 
+import com.rmompati.lang.frontend.Token;
+import com.rmompati.lang.intermediate.TypeFactory;
+import com.rmompati.lang.intermediate.TypeSpec;
 import com.rmompati.lang.pascal.frontend.PascalParserTD;
+import com.rmompati.lang.pascal.frontend.PascalTokenType;
+
+import java.util.EnumSet;
+
+import static com.rmompati.lang.intermediate.symtableimpl.DefinitionImpl.FIELD;
+import static com.rmompati.lang.intermediate.typeimpl.TypeFormImpl.RECORD;
+import static com.rmompati.lang.intermediate.typeimpl.TypeKeyImpl.RECORD_SYMTAB;
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.END;
+import static com.rmompati.lang.pascal.frontend.PascalTokenType.SEMICOLON;
+import static com.rmompati.lang.pascal.frontend.error.PascalErrorCode.MISSING_END;
 
 public class RecordTypeParser extends TypeSpecificationParser {
+
+  /** Synchronization set for the "END" token. */
+  private static final EnumSet<PascalTokenType> END_SET =
+      DeclarationsParser.VAR_START_SET.clone();
+  static {
+    END_SET.add(END);
+    END_SET.add(SEMICOLON);
+  }
 
   /**
    * Constructor.
@@ -11,5 +32,39 @@ public class RecordTypeParser extends TypeSpecificationParser {
    */
   public RecordTypeParser(PascalParserTD parent) {
     super(parent);
+  }
+
+  /**
+   * Parses a Pascal type specification.
+   *
+   * @param token the current token.
+   * @return the type specification.
+   * @throws Exception if an error occurs.
+   */
+  @Override
+  public TypeSpec parse(Token token) throws Exception {
+    TypeSpec recordType = TypeFactory.createType(RECORD);
+    token = nextToken(); // Consume the "RECORD".
+
+    // Push a symbol table for the RECORD type specification.
+    recordType.setAttribute(RECORD_SYMTAB, symTabStack.push());
+
+    // Parse the field declaration.
+    VariableDeclarationsParser variableDeclarationsParser = new VariableDeclarationsParser(this);
+    variableDeclarationsParser.setDefinition(FIELD);
+    variableDeclarationsParser.parse(token);
+
+    // Pop off the record's symbol table.
+    symTabStack.pop();
+
+    token = synchronize(END_SET);
+
+    if (token.getType() == END) {
+      token = nextToken();
+    } else {
+      errorHandler.flag(token, MISSING_END, this);
+    }
+
+    return recordType;
   }
 }
