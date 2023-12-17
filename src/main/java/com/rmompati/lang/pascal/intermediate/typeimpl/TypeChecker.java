2902b7191ac94fe4cdda8d068a057feafbbd97d1
MKeetile <mkeetile@csir.co.za>
2023-12-17 12:19:49 +0200

    Add two new type compatibilty methods.

--- src/main/java/com/rmompati/lang/pascal/intermediate/typeimpl/TypeChecker.java
+++ src/main/java/com/rmompati/lang/pascal/intermediate/typeimpl/TypeChecker.java
@@ -1,7 +1,11 @@
 package com.rmompati.lang.pascal.intermediate.typeimpl;
 
+import com.rmompati.lang.intermediate.TypeForm;
 import com.rmompati.lang.intermediate.TypeSpec;
 import com.rmompati.lang.pascal.intermediate.symtableimpl.Predefined;
+
+import static com.rmompati.lang.pascal.intermediate.typeimpl.TypeFormImpl.ENUMERATION;
+import static com.rmompati.lang.pascal.intermediate.typeimpl.TypeFormImpl.SCALAR;
 
 /**
  * <h1>TypeChecker</h1>
@@ -86,5 +90,56 @@ public class TypeChecker {
    */
   public static boolean isChar(TypeSpec type) {
     return (type != null) && (type.baseType() == Predefined.charType);
+  }
+
+  /**
+   * Checks if two type specifications are assignment compatible.
+   * @param targetType the target type specification.
+   * @param valueType the value type specification.
+   * @return true if the value can be assigned to the target, false otherwise.
+   */
+  public static boolean areAssignmentCompatible(TypeSpec targetType, TypeSpec valueType) {
+    if ((targetType == null) || (valueType == null)) {
+      return false;
+    }
+
+    targetType = targetType.baseType();
+    valueType = valueType.baseType();
+
+    boolean compatible = false;
+
+    // Identical types.
+    if (targetType == valueType) {
+      compatible =  true;
+    } else if (isReal(targetType) && isInteger(valueType)) {
+      compatible = true;
+    } else {
+      compatible = targetType.isPascalString() && valueType.isPascalString();
+    }
+
+    return compatible;
+  }
+
+  public static boolean areComparisonCompatible(TypeSpec type1, TypeSpec type2) {
+    if ((type1 == null) || (type2 == null)) {
+      return false;
+    }
+
+    type1 = type1.baseType();
+    type2 = type2.baseType();
+    TypeForm form = type1.getForm();
+    
+    boolean compatible = false;
+    
+    // Two identical scala or enumeration types.
+    if ((type1 == type2) && ((form == SCALAR) || (form == ENUMERATION))) {
+      compatible = true;
+    } else if (isAtLeastOneReal(type1, type2)) {
+      compatible = true;
+    } else {
+      compatible = type1.isPascalString() && type2.isPascalString();
+    }
+
+    return compatible;
   }
 }
